83f22bc286611814160d0552b431d57b31299acc
DESKTOP-BVUTE1A\fumiy <fumiya0903613@gmail.com>
2022-11-15 11:10:12 +0900

    api fix

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('mMembers', [App\Http\Controllers\MMemberController::class, 'get']);
-Route::get('mMembers/search', [App\Http\Controllers\MMemberController::class, 'search']);
 Route::post('mMembers/store', [App\Http\Controllers\MMemberController::class, 'store']);
 Route::post('mMembers/update', [App\Http\Controllers\MMemberController::class, 'update']);
