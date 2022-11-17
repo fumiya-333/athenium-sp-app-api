dc4467a69d33806710ef1ca340d925e504311061
DESKTOP-BVUTE1A\fumiy <fumiya0903613@gmail.com>
2022-11-17 12:33:06 +0900

    response fix

--- app/Providers/ApiResponseServiceProvider.php
+++ app/Providers/ApiResponseServiceProvider.php
@@ -16,23 +16,11 @@ class ApiResponseServiceProvider extends ServiceProvider
     public function boot()
     {
         Response::macro(AppConstants::KEY_SUCCESS, function ($response) {
-            return response()->json(
-                [
-                    AppConstants::KEY_SUCCESS => true,
-                    AppConstants::KEY_RESPONSE => $response,
-                ],
-                200
-            );
+            return response()->json($response, 200);
         });
 
         Response::macro(AppConstants::KEY_ERROR, function ($error, $error_code) {
-            return response()->json(
-                [
-                    AppConstants::KEY_SUCCESS => false,
-                    AppConstants::KEY_RESPONSE => $error,
-                ],
-                $error_code
-            );
+            return response()->json($error, $error_code);
         });
     }
 }
